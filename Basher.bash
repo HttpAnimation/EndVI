7cab7925c56e58cbbed6a45def4fec8ee6e4e03e
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-02-25 18:54:15 -0700

    Create Basher.bash

---
+++ Basher.bash
@@ -0,0 +1,8 @@
+mkdir ENDVI
+cd ENDVI
+git clone -b gh-pages https://github.com/HttpAnimation/EndVI.git
+mv EndVI gh-pages
+git clone -b blank https://github.com/HttpAnimation/EndVI.git
+mv EndVI blank
+git clone -b main https://github.com/HttpAnimation/EndVI.git
+mv EndVI main
